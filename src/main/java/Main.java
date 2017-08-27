b9e31983a149c2aa0fee5945c59713debe501cd8
Sumit Maji <skmaji1@outlook.com>
2017-08-27 08:17:25 +0000

    Removed unused imports.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,11 @@
 import java.util.Arrays;
-import java.util.Iterator;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
-
-import scala.Tuple2;
 
 public class Main {
     
@@ -31,6 +24,7 @@ public class Main {
         });
         
         log.info("The data is: "+StringUtils.join(rdd1.collect(), ","));
+        log.info(rdd1.collect());        
 
     }
 }
